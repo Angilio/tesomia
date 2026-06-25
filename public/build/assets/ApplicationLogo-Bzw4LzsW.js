4f21071be50f71c474a5cdcb216ce9ffb3b00b2d
Angilio <johntroaikkyman@gmail.com>
2026-06-25 23:52:46 +0300

    Ajuster Exportation pdf attributions avec build

--- public/build/assets/ApplicationLogo-Bzw4LzsW.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-BpGU61_3.js";function t(r){return o.jsx("h1",{children:"TESOMIA"})}export{t as A};
---
+++ public/build/assets/ApplicationLogo-WbsvYeOB.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-CMP-OJpj.js";function t(r){return o.jsx("h1",{children:"TESOMIA"})}export{t as A};
